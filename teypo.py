c8053eccf9d40eb1aad6e8573d8bdcfce0ab07ac
KingBSK <absk2415@gmail.com>
2024-10-19 16:32:58 +0530

    converted files

--- teypo.py
+++ teypo.py
@@ -1,5 +1,26 @@
 import pandas as pd
+from preprocessing import get_patients,get_events_and_epochs,get_lame_list
 
-df=pd.read_csv('temp.csv')
+df=pd.read_csv('participants.tsv',delimiter='\t')
+
+# print(df["Group"].unique())
  
-print(df.shape)
+df["Group"]=df["Group"].map({'A':0,'C':1,'F':2})
+
+
+
+lamelist=get_lame_list()
+lamelist.append(32)
+lamelist.append(40)
+sorted(lamelist)
+
+lise=[]
+
+
+for i in range(88):
+    if i in lamelist:
+        continue
+    lise.append(df["Group"][i])
+
+df1=pd.DataFrame(data=lise,columns=[1])
+df1.to_csv('class.csv')
